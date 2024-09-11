0b512d55b6cc4dde4a3dac5ac4755a75eeb46301
Sebati HM <sebashermon003@gmail.com>
2024-09-10 18:10:41 -0600

    DIP

--- Bloque 1/Interface Dependency/zuzzet/DIP Refactoring/src/main/java/org/example/AppConfig.java
+++ Bloque 1/Interface Dependency/zuzzet/DIP Refactoring/src/main/java/org/example/AppConfig.java
@@ -4,6 +4,8 @@ public class AppConfig {
     public static DocumentCreator generateDocumentGenerator() {
         FileCreatorRegistry registry = new FileCreatorRegistry();
         registry.registerCreators(1, new TxtFileCreator(), TxtFileCreator.getType());
+        registry.registerCreators(2, new DocxFileCreator(), DocxFileCreator.getType());
+        
 
         InputReader inputReader = new ConsoleReader();
         return new DocumentCreator(inputReader, registry);
